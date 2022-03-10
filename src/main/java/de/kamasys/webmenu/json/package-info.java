e260ecbca1e591b051779c93bf6e104af24eed41
Patrick Lindenberg <heapdump@icloud.com>
2022-03-10 16:15:59 +0100

    add package

---
+++ src/main/java/de/kamasys/webmenu/json/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains automatically generated POJOs
+ * based on: <a href="file:../data/menu.json">menu.json</a>
+ * as example mapping data.
+ */
+package de.kamasys.webmenu.json;
