b626155b204429669b68e587bf72ce19df95ea6e
Patrick Lindenberg <heapdump@icloud.com>
2022-03-10 18:11:58 +0100

    initial commit

--- src/main/java/de/kamasys/webmenu/json/package-info.java
+++ src/main/java/de/kamasys/webmenu/json/package-info.java
@@ -1,6 +1,5 @@
 /**
  * This package contains automatically generated POJOs
- * based on: <a href="file:../data/menu.json">menu.json</a>
- * as example mapping data.
+ * based on json data: <a href="file:../data/menu.json">menu.json</a>.
  */
 package de.kamasys.webmenu.json;
