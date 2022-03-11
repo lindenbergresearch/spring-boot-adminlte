1c62ba5a77650f2a4c402963b040f59b0b607190
Patrick Lindenberg <heapdump@icloud.com>
2022-03-11 21:32:31 +0100

    rename

--- src/main/java/de/kamasys/webmenu/SystemApplication.java
+++ src/main/java/de/kamasys/webmenu/SystemApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemApplication {
+public class SystemApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SistemApplication.class, args);
+    public static void main(String... args) {
+        SpringApplication.run(SystemApplication.class, args);
     }
 }
