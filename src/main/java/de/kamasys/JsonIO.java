1b002881fba9190a3325701b045709648f67106e
Patrick Lindenberg <heapdump@icloud.com>
2022-03-15 11:08:50 +0100

    update doc and fix json io helper

--- src/main/java/de/kamasys/JsonIO.java
+++ src/main/java/de/kamasys/JsonIO.java
@@ -1,53 +1,51 @@
-package de.kamasys.webmenu;
+package de.kamasys;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import de.kamasys.webmenu.json.JsonRoot;
-
 
 import java.io.*;
 
 /**
- *
+ * JSON helper IO methods.
  */
 public class JsonIO {
 
     /**
-     * @param json
-     * @return
+     * Converts a JSON compliant string into a
+     * hierarchic POJO structure.
+     *
+     * @param json JSON as string.
+     * @return The root instance of the converted structure.
+     * @throws JsonSyntaxException Raised on malformed JSON.
      */
-    public static JsonRoot createFromString(String json) {
-        Gson gson = new Gson();
-        JsonRoot root = gson.fromJson(json, JsonRoot.class);
-
-        //TODO handle io exception?
-
-        return root;
+    public static JsonRoot createFromString(String json) throws JsonSyntaxException {
+        return new Gson().fromJson(json, JsonRoot.class);
     }
 
 
     /**
-     * @param file
-     * @return
-     * @throws IOException
+     * Loads a JSON file and converts it to a
+     * hierarchic POJO structure.
+     *
+     * @param file The path to a valid JSON file.
+     * @return The root instance of the converted structure.
+     * @throws IOException         Raised on common IO failure.
+     * @throws JsonSyntaxException Raised on malformed JSON.
      */
-    public static JsonRoot createFromFile(String file) throws IOException {
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
+    public static JsonRoot createFromFile(String file) throws IOException, JsonSyntaxException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        StringBuilder sb = new StringBuilder();
+        String line = br.readLine();
 
-            while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
-                line = br.readLine();
-            }
-
-            if (sb.length() > 0)
-                return createFromString(sb.toString());
-
-            return null;
+        while (line != null) {
+            sb.append(line);
+            sb.append(System.lineSeparator());
+            line = br.readLine();
         }
 
+        if (sb.length() > 0)
+            return createFromString(sb.toString());
+
+        throw new JsonSyntaxException("Empty JSON file.");
     }
-
-
 }
